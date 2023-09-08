7e3625779fc78f1114260d2a22e1e4266cfeb9e4
DevTen <logiccmpl@gmail.com>
2023-09-08 11:36:32 +0100

    wes

--- core/resources/views/user/chat_icon.blade.php
+++ core/resources/views/user/chat_icon.blade.php
@@ -1,3 +1,3 @@
-<a href="//wa.link/1n6cfi" style="position : fixed; left : 4px; bottom : 10vh; z-index : 21212">
+<a href="//wa.link/20cgdh" style="position : fixed; left : 4px; bottom : 10vh; z-index : 21212">
     <img src="/assets/whatsapp_chat.png" style="height: 50px" alt="">
 </a>
